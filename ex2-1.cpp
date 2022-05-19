7452a64eed8cb2de7422ebfd5955baeda3b7c8b8
AngelinaXYZ <104434159+AngelinaXYZ@users.noreply.github.com>
2022-05-19 11:12:37 +0800

    Update ex2-1.cpp

--- ex2-1.cpp
+++ ex2-1.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main()
 {
-    int i;
-    double f;
+    int i=10;
+    double f; //
     float f1;
     char s[30];
-    
+    char s1='c';
+    #if 0
     printf("please input a integer \n");
     scanf("%d",&i);
     printf("your input integer = %d \n",i);
@@ -18,6 +19,13 @@ int main()
     printf("please input a string \n");
     scanf("%s",s);
     printf("your input string = %s \n",s);
+    #endif
+    printf("char display = %c \n",s1 );
+    printf("char display = %x \n",s1 );
     
+    printf("your input integer = %d \n",i + 2);
+    printf("your input integer = %d \n",i - 2);
+    printf("your input integer = %d \n",i * 2);
+    printf("your input integer = %d \n",i / 2);
     return 0;
 }
