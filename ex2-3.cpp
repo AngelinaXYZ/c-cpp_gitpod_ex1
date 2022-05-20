1a2c706a7d8af02d5c68dacda8b9992669b3bd02
AngelinaXYZ <104434159+AngelinaXYZ@users.noreply.github.com>
2022-05-20 13:39:50 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,15 @@
+//題目: 輸入兩個整數，求兩數的平方和並輸出
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cout<<"please input two numbers ";
+    cin>>a>>b;
+    cout<<"your input numbers are "<<a<<" & "<<b<<endl;
+    c = a*a;
+    d = b*b;
+    cout<<"the square are "<<c<<" & "<<d;
+    return 0;
+}
